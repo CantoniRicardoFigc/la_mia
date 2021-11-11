03d9c03858bb601113f7f8c72b51286f137f49bc
CantoniRicardoFigc <cantoni.riccardo@iisgalvanimi.edu.it>
2021-11-11 11:55:09 +0000

    nuova versione

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function buttonFunction() {
-
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
 
     ctx.fillStyle = "#ff8000";
     ctx.fillRect(0, 0, 150, 150);
-
-  }
+}
 
 function button2Function() {
-
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
 
@@ -18,14 +15,21 @@ function button2Function() {
     for (let index = 0; index <= 140; index = index +20) {
         ctx.fillRect(index,index, 10, 10)
     }
-    
-    ctx.fillRect(140, 0, 10, 10);
-    ctx.fillRect(120, 20, 10, 10);
-    ctx.fillRect(100, 40, 10, 10);
-    ctx.fillRect(80, 60, 10, 10);
-    ctx.fillRect(60, 80, 10, 10);
-    ctx.fillRect(40, 100, 10, 10);
-    ctx.fillRect(20, 120, 10, 10);
-    ctx.fillRect(0, 140, 10, 10);
+    for (let index = 140; index >= 0; index = index -20) {
+        let z=140;
+        ctx.fillRect(index,z-index, 10, 10)
+    }
+}
 
+function button3Function() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "#ff0000";
+
+    for (let index = 140; index >= 0; index = index -20) {
+        for (let j = 0; j <=140; j = j +20) {
+        ctx.fillRect(index,j, 10, 10)
+        }
+    }
 }
