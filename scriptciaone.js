bcb759b8f10955012fa6c6d9b5b6c925910c5cef
CantoniRicardoFigc <cantoni.riccardo@iisgalvanimi.edu.it>
2021-11-12 10:55:36 +0000

    versione ufficiale

--- scriptciaone.js
+++ scriptciaone.js
@@ -1,16 +1,14 @@
-let scambio=false;
+let i = 0;
 function buttonCiaone() {
+    i++;
     document.getElementById("h").innerHTML = "Prontone"
     document.getElementById("b").innerHTML = "Disegna"
     
-    if (scambio==false) {
-        scambio==true
-    }
-    
-    if (document.getElementById("b").innerHTML == "Disegna" && scambio==false) {
+    if (i==2) {
         var canvas = document.getElementById("myCanvas1");
         var ctx = canvas.getContext("2d");
         ctx.fillStyle = "#ff0000";
         ctx.fillRect(0, 0, 500, 500);
     }
+    
 }
