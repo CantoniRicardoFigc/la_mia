9cc58a74bfb0bfac36f6102f90ffab442a459c58
CantoniRicardoFigc <cantoni.riccardo@iisgalvanimi.edu.it>
2021-11-12 12:49:28 +0000

    versione official 1.0

--- scriptciaone.js
+++ scriptciaone.js
@@ -1,21 +1,15 @@
 let i = 0;
+
 function buttonCiaone() {
+
     i++;
     document.getElementById("h").innerHTML = "Prontone"
     document.getElementById("b").innerHTML = "Disegna"
     
     if (i==2) {
-        var canvas = document.getElementById("myCanvas1");
-        var ctx = canvas.getContext("2d");
-        ctx.fillStyle = "#ff0000";
-        ctx.fillRect(0, 0, 300, 300);
         document.getElementById("b").innerHTML = "Ricomincia"
     }
     if (i==3) {
-        var canvas = document.getElementById("myCanvas1");
-        var ctx = canvas.getContext("2d");
-        ctx.fillStyle = "#ffffff";
-        ctx.fillRect(0, 0, 300, 300);
         document.getElementById("b").innerHTML = "Cambia testo"
         document.getElementById("h").innerHTML = "Ciaone"
         i=0;
@@ -23,9 +17,11 @@ function buttonCiaone() {
     
 }
 function buttonQuadrati() {
+
     var canvas = document.getElementById("myCanvas1");
     var ctx = canvas.getContext("2d");
 
+    //rosso
     for (let index = 0; index <= 250; index = index +10) {
         ctx.fillStyle = "#ff0000";
         ctx.fillRect(index, 0, 20, 20)
@@ -43,6 +39,7 @@ function buttonQuadrati() {
         ctx.fillRect(index, 250, 20, 20)
     }
 
+    //arancione
     for (let index = 20; index <= 230; index = index +10) {
         ctx.fillStyle = "orange";
         ctx.fillRect(index,20, 20, 20)
@@ -60,6 +57,7 @@ function buttonQuadrati() {
         ctx.fillRect(index, 230, 20, 20)
     }
 
+    //giallo
     for (let index = 40; index <= 210; index = index +10) {
         ctx.fillStyle = "yellow";
         ctx.fillRect(index,40, 20, 20)
@@ -75,6 +73,24 @@ function buttonQuadrati() {
     for (let index = 40; index <= 210; index = index +10) {
         ctx.fillStyle = "yellow";
         ctx.fillRect(index, 210, 20, 20)
+    }
+
+    //verde
+    for (let index = 60; index <= 170; index = index +10) {
+        ctx.fillStyle = "lightgreen";
+        ctx.fillRect(index,60, 40, 40)
+    }
+    for (let index = 60; index <= 170; index = index +10) {
+        ctx.fillStyle = "lightgreen";
+        ctx.fillRect(60, index, 40, 40)
+    }
+    for (let index = 60; index <= 170; index = index +10) {
+        ctx.fillStyle = "lightgreen";
+        ctx.fillRect(170, index, 40, 40)
+    }
+    for (let index = 60; index <= 170; index = index +10) {
+        ctx.fillStyle = "lightgreen";
+        ctx.fillRect(index, 170, 40, 40)
     }
     
 }
