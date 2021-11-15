0f37a98c9ab990d06632e32c43f5a04ad05ba97b
CantoniRicardoFigc <cantoni.riccardo@iisgalvanimi.edu.it>
2021-11-15 11:23:49 +0000

    version plus

---
+++ scriptsaluti.js
@@ -0,0 +1,15 @@
+function genera() {
+    var ct = document.getElementById("testo"); // commenta qui
+    var console = document.getElementById("console"); // commenta qui
+    if(ct.value == "")
+        console.innerHTML = "inserire il valore massimo da generare.";
+    else {
+        var num_max = ct.value; // commenta qui
+        var num_generato = Math.floor((Math.random() * num_max) + 1); //commenta qui
+        console.innerHTML = " IL numero  casuale estratto è: " + num_generato; //commenta qui
+    }  
+}
+function resetta() {
+    document.getElementById("testo").value = " "
+    document.getElementById("console").innerHTML = " ";
+}
